2e77136d3adac6c1ba39fe0666bd355bdcdacca6
Shashankvjogi <52234269+Shashankvjogi@users.noreply.github.com>
2024-07-27 09:41:55 +0530

    Add files via upload

---
+++ day7/malloc/Day7main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Day7ins.c"
+//#include "day7.h"
+
+int main()
+{
+
+   int size=0;
+   scanf("%d",&size);
+   int *arr;
+   allot_memory(arr,size);
+
+   add_elements(arr,size);
+   printf("%d\n",sum(arr,size));
+
+   int max,min;
+
+   max_min(arr,size,&max,&min);
+   printf("%d %d\n",max,min);
+
+   
+
+  
+
+  
+
+
+}
